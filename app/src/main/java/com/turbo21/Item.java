baa4e498cb9be61d044ec21d2f1546b0125eddce
Celina Wong <celinawong25@gmail.com>
2025-03-26 11:29:43 -0600

    Initializing Item and Shop classes

---
+++ app/src/main/java/com/turbo21/Item.java
@@ -0,0 +1,19 @@
+package com.turbo21;
+
+public class Item {
+
+    public String name;
+    public String description;
+    public int cost;
+
+    public Item(String name, String description, int cost) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
+    public void UseItem() { // asbtract class
+        //
+    }
+
+
+}
