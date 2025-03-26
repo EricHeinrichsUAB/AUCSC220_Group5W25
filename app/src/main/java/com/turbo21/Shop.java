6eb9e6627994d5cc2c86a19aed18e26602575f87
Celina Wong <celinawong25@gmail.com>
2025-03-26 11:29:43 -0600

    Initializing Item and Shop classes

---
+++ app/src/main/java/com/turbo21/Shop.java
@@ -0,0 +1,22 @@
+package com.turbo21;
+
+import java.util.ArrayList;
+
+public class Shop {
+
+    public ArrayList<Item> Items;
+
+    public Shop {
+        ArrayList<Item> Items = new ArrayList<>(3);
+    }
+
+    public Item SellItem(int intZeroToTwo) {
+        return Items[intZeroToTwo];
+    }
+
+    public Void RefreshItems {
+        //
+    }
+
+
+}
