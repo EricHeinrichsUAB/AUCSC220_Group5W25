b86d5e49342deb759d7bb87563f59f05064d9793
Celina Wong <celinawong25@gmail.com>
2025-03-30 16:33:23 -0600

    Initializing Item and Shop classes

--- app/src/main/java/com/turbo21/Item.java
+++ app/src/main/java/com/turbo21/Item.java
@@ -2,12 +2,12 @@ package com.turbo21;
 
 public abstract class Item {
 
-    public String name;
+    public String nam;
     public String description;
     public int cost;
 
     public Item(String name, String description, int cost) {
-        this.name = name;
+        this.nam = nam;
         this.description = description;
         this.cost = cost;
     }
