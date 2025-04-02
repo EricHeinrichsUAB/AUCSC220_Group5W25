2b3ce08f9e67d3071cf33a6781b8b899066b104d
Celina Wong <celinawong25@gmail.com>
2025-04-02 13:25:23 -0600

    Started trying to implement methods (back-end) with play_screen.xml (UI) in order to get the main functionality of the game working.

--- app/src/main/java/com/turbo21/MainActivity.java
+++ app/src/main/java/com/turbo21/MainActivity.java
@@ -28,14 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
         playButton = findViewById(R.id.playButton);
 
-        // OnClickListener
+
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // Create an Intent to start the PlayActivity
+                // Remember to cite this code
                 Intent intent = new Intent(MainActivity.this, PlayActivity.class);
                 startActivity(intent);
+
 
             }//onClick
         });//setOnClickListener
