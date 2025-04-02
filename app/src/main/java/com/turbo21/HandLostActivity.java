d9101e166998b4fd206a9fac9341fe455f849fc4
PuffDanny12 <djt@ualberta.ca>
2025-04-02 12:29:06 -0600

    Renaming of Java activity files
    Names of Java classes renamed for consistency.

--- app/src/main/java/com/turbo21/HandLostActivity.java
+++ app/src/main/java/com/turbo21/HandLostActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-public class HandLost extends AppCompatActivity {
+public class HandLostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/turbo21/HandWonActivity.java
+++ app/src/main/java/com/turbo21/HandWonActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-public class Settings extends AppCompatActivity {
+public class HandWonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
