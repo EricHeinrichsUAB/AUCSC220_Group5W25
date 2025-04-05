836fc5dd09479a6d5b662fa4c81abcc21bebf252
PuffDanny12 <djt@ualberta.ca>
2025-04-05 15:38:52 -0600

    Functionality of buttons on HandLostActivity.java and HandWonActivity.java
    Buttons in hand won/lost screens now direct to appropriate pages on click.

--- app/src/main/java/com/turbo21/HandLostActivity.java
+++ app/src/main/java/com/turbo21/HandLostActivity.java
@@ -1,6 +1,9 @@
 package com.turbo21;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 public class HandLostActivity extends AppCompatActivity {
 
+    private Button menuButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.screen_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.screen_hand_lost);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.handLost), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        menuButton = findViewById(R.id.menuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SettingsActivity
+                Intent intent = new Intent(HandLostActivity.this, MainActivity.class);
+                startActivity(intent);
+                } //onClick
+            }); //setOnClickListener
     }
 }
--- app/src/main/java/com/turbo21/HandWonActivity.java
+++ app/src/main/java/com/turbo21/HandWonActivity.java
@@ -1,6 +1,10 @@
 package com.turbo21;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +13,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 public class HandWonActivity extends AppCompatActivity {
 
+    private Button playButton;
+    private Button menuButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.screen_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.screen_hand_won);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.handWon), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        playButton = findViewById(R.id.playButton);
+        menuButton = findViewById(R.id.menuButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the PlayActivity
+                Intent intent = new Intent(HandWonActivity.this, PlayActivity.class);
+                startActivity(intent);
+            } //onClick
+        }); //setOnClickListener
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SettingsActivity
+                Intent intent = new Intent(HandWonActivity.this, MainActivity.class);
+                startActivity(intent);
+                } //onClick
+            }); //setOnClickListener
     }
+
+
 }
