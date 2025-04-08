787bfc0c1c76b761ac1dbed9db22594db65143f3
Eric Heinrichs <163155204+EricHeinrichsUAB@users.noreply.github.com>
2025-04-08 16:24:38 -0600

    Remove unused imports

--- app/src/main/java/com/turbo21/HandWonActivity.java
+++ app/src/main/java/com/turbo21/HandWonActivity.java
@@ -3,11 +3,7 @@ package com.turbo21;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.view.View;
 import android.widget.Button;
