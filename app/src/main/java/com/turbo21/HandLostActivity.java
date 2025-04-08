7ad17c0df607162d2ca19636b2cda386150cec2d
Celina Wong <celinawong25@gmail.com>
2025-04-08 05:00:48 -0600

    Making Next and Quit buttons functional -Changed button id's -Works perfectly

--- app/src/main/java/com/turbo21/HandLostActivity.java
+++ app/src/main/java/com/turbo21/HandLostActivity.java
@@ -1,17 +1,35 @@
 package com.turbo21;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+
 public class HandLostActivity extends AppCompatActivity {
+
+    private Button handLostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_hand_lost);
+
+        handLostButton = findViewById(R.id.handLostButton);
+
+        handLostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to home screen
+                Intent intent = new Intent(HandLostActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/turbo21/HandWonActivity.java
+++ app/src/main/java/com/turbo21/HandWonActivity.java
@@ -1,5 +1,6 @@
 package com.turbo21;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,11 +8,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+
 public class HandWonActivity extends AppCompatActivity {
+
+    private Button quitButton;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_hand_won);
+
+        quitButton = findViewById(R.id.quitButton);
+        nextButton = findViewById(R.id.nextButton);
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to home screen
+                Intent intent = new Intent(HandWonActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to play screen
+                Intent intent = new Intent(HandWonActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
