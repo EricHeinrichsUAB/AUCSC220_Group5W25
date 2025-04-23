1564e3e1accf0d1d8bfdaa08ac9e84db3456af48
Eric Heinrichs <163155204+EricHeinrichsUAB@users.noreply.github.com>
2025-04-23 16:41:51 -0600

    Add per-run stats to the summary page

--- app/src/main/java/com/turbo21/HandLostActivity.java
+++ app/src/main/java/com/turbo21/HandLostActivity.java
@@ -10,10 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HandLostActivity extends AppCompatActivity {
 
     private Button handLostButton;
+    private TextView roundsPlayedText;
+    private TextView cardsDrawnText;
+    private TextView finalScoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,13 @@ public class HandLostActivity extends AppCompatActivity {
         setContentView(R.layout.screen_hand_lost);
 
         handLostButton = findViewById(R.id.handLostButton);
+        roundsPlayedText = findViewById(R.id.textRoundsPlayed);
+        cardsDrawnText = findViewById(R.id.textCardsDrawn);
+        finalScoreText = findViewById(R.id.textFinalScore);
+
+        roundsPlayedText.setText(String.format("Rounds Played: %s", GameManager.RoundNumber));
+        cardsDrawnText.setText(String.format("Cards Drawn: %s", GameManager.Player.cardsDrawn));
+        finalScoreText.setText(String.format("Final Score: %s", GameManager.Player.overallScore));
 
         handLostButton.setOnClickListener(new View.OnClickListener() {
             @Override
